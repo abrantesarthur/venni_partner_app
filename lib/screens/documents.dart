44a55639240f53ac7a4b14fd057db58f7e3fd84d
Arthur Abrantes <aabrantes@college.harvard.edu>
2021-06-01 22:35:46 -0300

    add banner

--- lib/screens/documents.dart
+++ lib/screens/documents.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:partner_app/models/connectivity.dart';
+import 'package:partner_app/styles.dart';
 import 'package:provider/provider.dart';
 
 class Documents extends StatefulWidget {
@@ -14,10 +15,54 @@ class DocumentsState extends State<Documents> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     ConnectivityModel connectivity = Provider.of<ConnectivityModel>(context);
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Stack(
-        children: [Center(child: Text("documents"))],
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight / 8,
+            color: AppColor.primaryPink,
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: screenHeight / 20,
+                right: screenWidth / 15,
+                left: screenWidth / 15,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage("images/horizontal-white-logo.png"),
+                    width: screenWidth * 0.3,
+                  ),
+                  Spacer(),
+                  Container(
+                    width: screenWidth / 4.5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(screenHeight / 100),
+                      child: Row(
+                        children: [
+                          Text("Ajuda"),
+                          Spacer(),
+                          Icon(
+                            Icons.keyboard_arrow_down,
+                            size: 18,
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
