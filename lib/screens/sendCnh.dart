1098b92df3822d99c7d2ec4761480b66157367b5
Arthur Abrantes <aabrantes@college.harvard.edu>
2021-06-07 16:06:11 -0300

    refactor crlv screen

--- lib/screens/sendCnh.dart
+++ lib/screens/sendCnh.dart
@@ -16,91 +16,6 @@ class SendCnhNumberState extends State<SendCnh> {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
-    Scaffold(
-      body: LayoutBuilder(
-        builder: (
-          BuildContext context,
-          BoxConstraints viewportConstraints,
-        ) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: viewportConstraints.maxHeight,
-              ),
-              child: OverallPadding(
-                child: IntrinsicHeight(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          ArrowBackButton(
-                              onTapCallback: () => Navigator.pop(context)),
-                          Spacer(),
-                        ],
-                      ),
-                      SizedBox(height: screenHeight / 25),
-                      Text(
-                        "Envie uma foto da sua Carteira Nacional de Habilitação (CNH) com EAR",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(height: screenHeight / 25),
-                      Text(
-                        "Requisitos de envio",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(height: screenHeight / 50),
-                      Text(
-                        "1. A CNH deve ter a observação Exerce Atividade Remunerada (EAR), estar legível e dentro do prazo de validade.",
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                      SizedBox(height: screenHeight / 50),
-                      Text(
-                        "2. A foto deve ser tirada do documento aberto e fora do plástico, como na ilustração abaixo",
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                      SizedBox(height: screenHeight / 50),
-                      Text(
-                        "3. Se preferir, pode enviar uma foto da CNH digital que você pode obter no site do Denatran ou através do app gov.b",
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                      SizedBox(height: screenHeight / 25),
-                      Row(
-                        children: [
-                          Spacer(),
-                          Container(
-                            width: screenWidth / 1.3,
-                            height: screenHeight / 4,
-                            alignment: Alignment.center,
-                            decoration: new BoxDecoration(
-                              image: new DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage("images/cnh.png")),
-                            ),
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                      SizedBox(height: screenHeight / 25),
-                      // TODO: name CRLV correctly based on type (image versus pdf)
-                      AppButton(textData: "Enviar CNH", onTapCallBack: () {}),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
     return Scaffold(
       backgroundColor: Colors.white,
       body: OverallPadding(
@@ -182,16 +97,7 @@ class SendCnhNumberState extends State<SendCnh> {
                     left: 0,
                     right: 0,
                     child: AppButton(
-                      child: Text(
-                        "Adicionar",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
-                          color: Colors.white,
-                        ),
-                      ),
-                      textData: "Adicionar",
+                      textData: "Enviar CNH",
                       buttonColor: AppColor.primaryPink,
                       onTapCallBack: () {},
                     ),
--- lib/screens/sendCrlv.dart
+++ lib/screens/sendCrlv.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:partner_app/styles.dart';
 import 'package:partner_app/widgets/appButton.dart';
 import 'package:partner_app/widgets/arrowBackButton.dart';
 import 'package:partner_app/widgets/overallPadding.dart';
@@ -17,88 +18,96 @@ class SendCrlvNumberState extends State<SendCrlv> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (
-          BuildContext context,
-          BoxConstraints viewportConstraints,
-        ) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: viewportConstraints.maxHeight,
-              ),
-              child: OverallPadding(
-                child: IntrinsicHeight(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          ArrowBackButton(
-                              onTapCallback: () => Navigator.pop(context)),
-                          Spacer(),
-                        ],
-                      ),
-                      SizedBox(height: screenHeight / 25),
-                      Text(
-                        "Envie o seu CRLV",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(height: screenHeight / 25),
-                      Text(
-                        "Requisitos de envio",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(height: screenHeight / 50),
-                      Text(
-                        "1. O documento do veículo deve estar dentro do prazo de validade",
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                      SizedBox(height: screenHeight / 50),
-                      Text(
-                        "2. Envie somente a primeira página. Porém, os dados devem estar legíveis",
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                      SizedBox(height: screenHeight / 50),
-                      Text(
-                        "3. Você pode enviar ou uma foto do CRLV físico ou o CRLV digital em formato PDF que você pode obter no site do Detran ou através do app gov.br",
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      ),
-                      SizedBox(height: screenHeight / 25),
-                      Row(
-                        children: [
-                          Spacer(),
-                          Container(
-                            width: screenWidth / 1.3,
-                            height: screenHeight / 4,
-                            alignment: Alignment.center,
-                            decoration: new BoxDecoration(
-                              image: new DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage("images/crlv.png")),
-                            ),
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                      SizedBox(height: screenHeight / 25),
-                      // TODO: name CRLV correctly based on type (image versus pdf)
-                      AppButton(textData: "Enviar CRLV", onTapCallBack: () {}),
-                    ],
-                  ),
+      backgroundColor: Colors.white,
+      body: OverallPadding(
+        bottom: 0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                ArrowBackButton(
+                  onTapCallback: () => Navigator.pop(context),
                 ),
+                Spacer(),
+              ],
+            ),
+            SizedBox(height: screenHeight / 25),
+            Text(
+              "Envie o seu CRLV",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
               ),
             ),
-          );
-        },
+            SizedBox(height: screenHeight / 25),
+            Expanded(
+              child: Stack(
+                children: [
+                  SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Requisitos de envio",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: screenHeight / 50),
+                        Text(
+                          "1. O documento do veículo deve estar dentro do prazo de validade",
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                        ),
+                        SizedBox(height: screenHeight / 50),
+                        Text(
+                          "2. Envie somente a primeira página. Porém, os dados devem estar legíveis",
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                        ),
+                        SizedBox(height: screenHeight / 50),
+                        Text(
+                          "3. Você pode enviar ou uma foto do CRLV físico ou o CRLV digital em formato PDF que você pode obter no site do Detran ou através do app gov.br",
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                        ),
+                        SizedBox(height: screenHeight / 25),
+                        Row(
+                          children: [
+                            Spacer(),
+                            Container(
+                              width: screenWidth / 1.3,
+                              height: screenHeight / 4,
+                              alignment: Alignment.center,
+                              decoration: new BoxDecoration(
+                                image: new DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage("images/crlv.png")),
+                              ),
+                            ),
+                            Spacer(),
+                          ],
+                        ),
+                        SizedBox(height: screenHeight / 5),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: screenHeight / 15,
+                    left: 0,
+                    right: 0,
+                    child: AppButton(
+                      textData: "Enviar CRLV",
+                      buttonColor: AppColor.primaryPink,
+                      onTapCallBack: () {},
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
